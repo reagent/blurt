a22d507f8ff3be8a7e9a7874bdcd450bb86c81d7
Patrick Reagan <patrick.reagan@viget.com>
2008-09-17 00:52:50 -0400

    Require authentication for all API methods

--- app/services/api/service.rb
+++ app/services/api/service.rb
@@ -4,21 +4,29 @@ module Api
     web_service_api Api::Definition
       
     def newPost(blogid, username, password, struct, publish)
-      post = ::Post.create(:title => struct['title'], :body => struct['description'])
-      post.id.to_s
+      Authenticator.with_authentication(username, password) do
+        post = ::Post.create(:title => struct['title'], :body => struct['description'])
+        post.id.to_s
+      end
     end
  
     def getPost(postid, username, password)
-      Api::Struct::Post.from_active_record(Post.find(postid))
+      Authenticator.with_authentication(username, password) do
+        Api::Struct::Post.from_active_record(Post.find(postid))
+      end
     end
     
     def editPost(post_id, username, password, struct, publish)
-      Post.find(post_id).update_attributes(:title => struct['title'], :body => struct['description'])
-      true
+      Authenticator.with_authentication(username, password) do
+        Post.find(post_id).update_attributes(:title => struct['title'], :body => struct['description'])
+        true
+      end
     end
     
     def getRecentPosts(blog_id, username, password, limit)
-      Post.find(:all, :limit => limit, :order => 'created_at DESC').map {|post| Api::Struct::Post.from_active_record(post) }
+      Authenticator.with_authentication(username, password) do
+        Post.find(:all, :limit => limit, :order => 'created_at DESC').map {|post| Api::Struct::Post.from_active_record(post) }
+      end
     end
   
   end
--- test/unit/api/service_test.rb
+++ test/unit/api/service_test.rb
@@ -5,7 +5,10 @@ class Api::ServiceTest < ActiveSupport::TestCase
   context "An instance of Api::Service" do
 
     setup do
+      @username = 'reagent'
+      @password = 'fluffybunnies'
       @attributes = {:title => 'Title', :description => 'Description'}
+
       @service = Api::Service.new
       @post_struct = Api::Struct::Post.new(@attributes)
     end
@@ -13,25 +16,47 @@ class Api::ServiceTest < ActiveSupport::TestCase
     context "when creating a new post" do
 
       setup do
-        @initial_count = Post.count
-        @response = @service.newPost(0, 'username', 'password', @post_struct, true)
-        @new_post = Post.last
+        @post_count = Post.count
+        @new_post_method = lambda { @service.newPost(0, @username, @password, @post_struct, true) }
       end
 
-      should "increment the post count" do
-        assert_equal @initial_count + 1, Post.count
+      context "as an authenticated user" do
+        setup do
+          Authenticator.stubs(:authenticated?).with(@username, @password).returns(true)
+          @response = @new_post_method.call
+          @new_post = Post.last
+        end
+
+        should "increment the post count" do
+          assert_equal @post_count + 1, Post.count
+        end
+
+        should "return the ID of the new post" do
+          assert_equal @new_post.id.to_s, @response
+        end
+
+        should "assign the proper :title to the new post" do
+          assert_equal @attributes[:title], @new_post.title
+        end
+
+        should "assign the proper :body to the new post" do
+          assert_equal @attributes[:description], @new_post.body
+        end
       end
 
-      should "return the ID of the new post" do
-        assert_equal @new_post.id.to_s, @response
-      end
+      context "as an unauthenticated user" do
+        setup do
+          Authenticator.stubs(:authenticated?).with(@username, @password).returns(false)
+          @response = @new_post_method.call
+        end
 
-      should "assign the proper :title to the new post" do
-        assert_equal @attributes[:title], @new_post.title
-      end
+        should "not increment the post count" do
+          assert_equal @post_count, Post.count
+        end
 
-      should "assign the proper :body to the new post" do
-        assert_equal @attributes[:description], @new_post.body
+        should "return nil" do
+          assert_nil @response
+        end
       end
 
     end
@@ -39,16 +64,35 @@ class Api::ServiceTest < ActiveSupport::TestCase
     context "when retrieving an existing post" do
 
       setup do
-        post = Factory(:post, :title => 'Title', :body => 'Body')
-        @response = @service.getPost(post.id, 'username', 'password')
+        @get_post_method = lambda {|post| @service.getPost(post.id, @username, @password) }
+        @post = Factory(:post, :title => 'Title', :body => 'Body')
       end
 
-      should "have the correct title" do
-        assert_equal 'Title', @response['title']
+      context "as an authenticated user" do
+        setup do
+          Authenticator.stubs(:authenticated?).with(@username, @password).returns(true)
+          @response = @get_post_method.call(@post)
+        end
+
+        should "have the correct title" do
+          assert_equal 'Title', @response['title']
+        end
+
+        should "have the correct :description" do
+          assert_equal 'Body', @response['description']
+        end
       end
 
-      should "have the correct :description" do
-        assert_equal 'Body', @response['description']
+      context "as an unauthenticated user" do
+        setup do
+          Authenticator.stubs(:authenticated?).with(@username, @password).returns(false)
+          @response = @get_post_method.call(@post)
+        end
+
+        should "return nil" do
+          assert_nil @response
+        end
+
       end
 
     end
@@ -56,18 +100,39 @@ class Api::ServiceTest < ActiveSupport::TestCase
     context "when editing an existing post" do
 
       setup do
-        post = Factory(:post, :title => 'Title', :body => 'Body')
-        @method = lambda { @response = @service.editPost(post.id, 'username', 'password', @post_struct, true) }
+        @edit_post_method = lambda {|post| @service.editPost(post.id, @username, @password, @post_struct, true) }
+        @post = Factory(:post, :title => 'Title', :body => 'Body')
       end
 
-      should "return true" do
-        @method.call
-        assert_equal true, @response
+      context "as an authenticated user" do
+        setup do
+          Authenticator.stubs(:authenticated?).with(@username, @password).returns(true)
+        end
+
+        should "return true" do
+          assert_equal true, @edit_post_method.call(@post)
+        end
+
+        should "update the existing post" do
+          Post.any_instance.expects(:update_attributes).with(:title => @attributes[:title], :body => @attributes[:description])
+          @edit_post_method.call(@post)
+        end
       end
 
-      should "update the existing post" do
-        Post.any_instance.expects(:update_attributes).with(:title => @attributes[:title], :body => @attributes[:description])
-        @method.call
+      context "as an unauthenticated user" do
+        setup do
+          Authenticator.stubs(:authenticated?).with(@username, @password).returns(false)
+        end
+
+        should "return nil" do
+          assert_nil @edit_post_method.call(@post)
+        end
+
+        should "not update any posts" do
+          Post.any_instance.expects(:update_attributes).with(kind_of(Hash)).never
+          @edit_post_method.call(@post)
+        end
+
       end
 
     end
@@ -75,47 +140,71 @@ class Api::ServiceTest < ActiveSupport::TestCase
     context "when retrieving latest posts" do
 
       setup do
-        @title = 'This is a title'
-        @body  = 'This is a body'
+        @get_recent_posts_method = lambda {|post_count| @service.getRecentPosts(0, @username, @password, post_count)}
       end
 
-      context "with a single post" do
+      context "as an authenticated user" do
+
         setup do
-          @post  = Factory(:post, :title => @title, :body => @body)
+          Authenticator.stubs(:authenticated?).with(@username, @password).returns(true)
+          
+          @title = 'This is a title'
+          @body  = 'This is a body'
         end
 
-        should "contain an array of posts" do
-          assert_kind_of Array, @service.getRecentPosts(0, 'username', 'password', 1)
+        context "with a single post" do
+          setup do
+            @post  = Factory(:post, :title => @title, :body => @body)
+          end
+
+          should "contain an array of posts" do
+            assert_kind_of Array, @get_recent_posts_method.call(1)
+          end
+
+          should "match the list of available posts" do
+            posts = @get_recent_posts_method.call(1)
+            assert_equal [@title, @body], [posts.first['title'], posts.first['description']]
+          end
         end
 
-        should "match the list of available posts" do
-          posts = @service.getRecentPosts(0, 'username', 'password', 1)
-          assert_equal [@title, @body], [posts.first['title'], posts.first['description']]
+        context "with multiple posts" do
+          setup do
+            2.times do |i|
+              post = Factory(:post, :title => i.to_s)
+              post.update_attribute(:created_at, i.day.from_now)
+            end
+          end
+
+          should "return only the specified number of posts" do
+            assert_equal 1, @get_recent_posts_method.call(1).length
+          end
+
+          should "return the most recent post first" do
+            most_recent_post = Post.find(:first, :order => 'created_at DESC')
+            posts = @get_recent_posts_method.call(Post.count)
+
+            assert_equal most_recent_post.title, posts.first['title']
+          end
+
         end
       end
       
-      context "with multiple posts" do
-        setup do
-          2.times do |i|
-            post = Factory(:post, :title => i.to_s)
-            post.update_attribute(:created_at, i.day.from_now)
-          end
-        end
-        
-        should "return only the specified number of posts" do
-          assert_equal 1, @service.getRecentPosts(0, 'username', 'password', 1).length
-        end
-        
-        should "return the most recent post first" do
-          most_recent_post = Post.find(:first, :order => 'created_at DESC')
-          posts = @service.getRecentPosts(0, 'username', 'password', Post.count)
+      context "as an unauthenticated user" do
 
-          assert_equal most_recent_post.title, posts.first['title']
+        setup do
+          Factory(:post)
+          Authenticator.stubs(:authenticated?).with(@username, @password).returns(false)
         end
-        
+
+        should "return nil" do
+          assert_nil @get_recent_posts_method.call(1)
+        end
+
       end
       
     end
+    
+
 
   end
 
