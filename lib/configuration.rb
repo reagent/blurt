74208467f8ee8cb58ce36d4ae511710d57cb8c4e
Patrick Reagan <patrick.reagan@viget.com>
2008-11-15 15:20:12 -0500

    Refactored reading of config values from YAML file

--- lib/configuration.rb
+++ lib/configuration.rb
@@ -1,49 +1,59 @@
-class Configuration 
+module ConfigurationReader
   
+  module ClassMethods
+    def configuration_reader(*keys)
+      keys.each do |key|
+        define_method key do
+          @configuration[key.to_s]
+        end
+      end
+    end
+  end
+  
+  module InstanceMethods
+    def initialize(configuration)
+      @configuration = configuration
+    end
+  end
+  
+end
+
+class Configuration 
+
   def self.from_file(file)
     @configuration ||= YAML.load_file(file)
   end
-  
+
   def self.application
     Application.new(@configuration['application'])
   end
-  
+
   def self.authentication
     Authentication.new(@configuration['authentication'])
   end
 
   class Application
+
+    extend ConfigurationReader::ClassMethods
+    include ConfigurationReader::InstanceMethods
     
-    def initialize(configuration)
-      @configuration = configuration
-    end
-    
+    configuration_reader :theme
+
     def base_url
       port_spec = ":#{@configuration['port']}" if (@configuration['port'] && @configuration['port'].to_s != '80')
       "http://#{@configuration['host']}#{port_spec}/"
     end
-    
-    def theme
-      @configuration['theme']
-    end
-    
+
   end
-  
+
   class Authentication
-    
-    def initialize(configuration)
-      @configuration = configuration
-    end
-    
-    def username
-      @configuration['username']
-    end
-    
-    def password
-      @configuration['password']
-    end
-    
+
+    extend ConfigurationReader::ClassMethods
+    include ConfigurationReader::InstanceMethods
+
+    configuration_reader :username, :password
+
   end
-  
+
 end
 
