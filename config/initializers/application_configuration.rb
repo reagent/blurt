9207739723e814a54c599c3e0cff6250b96a080f
Patrick Reagan <patrick.reagan@viget.com>
2008-11-21 21:10:26 -0500

    Don't set port for URL generation if not needed

--- config/initializers/application_configuration.rb
+++ config/initializers/application_configuration.rb
@@ -2,20 +2,23 @@ if RAILS_ENV == 'test'
   THEME_PATH = "#{RAILS_ROOT}/app/themes/default"
 else
   require "#{RAILS_ROOT}/lib/configuration"
-  
+
   Configuration.from_file "#{RAILS_ROOT}/config/blurt.yml"
 
   THEME_PATH = "#{RAILS_ROOT}/app/themes/#{Configuration.application.theme}"
 
   FileUtils.rm_rf("#{RAILS_ROOT}/public") if File.exist?("#{RAILS_ROOT}/public")
   File.symlink("#{THEME_PATH}/assets", "#{RAILS_ROOT}/public")
-  
+
   ::ActionController::UrlWriter.module_eval do
     default_url_options[:host]     = Configuration.application.host
-    default_url_options[:port]     = Configuration.application.port
     default_url_options[:protocol] = 'http'
+
+    unless Configuration.application.port.nil?
+      default_url_options[:port] = Configuration.application.port
+    end
   end
-  
+
 end
 
 class ActionController::Base
