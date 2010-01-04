9174f7b09ffc37e66d27032c26e63dfc60bf91b8
Patrick Reagan <patrick.reagan@viget.com>
2010-01-03 23:07:15 -0500

    Started conversion to modular app

--- application.rb
+++ application.rb
@@ -1,5 +1,5 @@
 require 'rubygems'
-require 'sinatra'
+require 'sinatra/base'
 
 require 'fileutils'
 
@@ -63,82 +63,84 @@ Blurt.setup do |config|
   config.credentials = 'user:asdf'
 end
 
-set :views, File.dirname(__FILE__) + '/themes/default/views'
-set :public, File.dirname(__FILE__) + '/themes/default/assets'
+module Blurt
+  class Application < Sinatra::Base
 
-helpers do
-  include Rack::Utils
-  alias_method :h, :escape_html
-  
-  def link_to(content, url)
-    "<a href=\"#{url}\">#{h(content)}</a>"
+    enable :static
+    set :views, 'themes/default/views'
+    set :public, 'themes/default/assets'
+
+    def title
+      @title ||= Title.new
+    end
+
+    helpers do
+      include Rack::Utils
+      alias_method :h, :escape_html
+
+      def link_to(content, url)
+        "<a href=\"#{url}\">#{h(content)}</a>"
+      end
+    end
+
+    get '/uploads/*' do |path_to_file|
+      full_path          = "#{Blurt.configuration.upload_path}/#{path_to_file}"
+      send_file full_path, :disposition => nil
+    end
+
+    get '/feed' do
+      content_type 'application/xml'
+
+      @posts = Post.for_page(1)
+      builder :posts
+    end
+
+    get '/sitemap.xml' do
+      content_type 'text/xml'
+
+      sitemap = Sitemap.new
+      sitemap.to_xml
+    end
+
+    get '/tags' do
+      title.prepend 'tags'
+      @tags = Tag.by_name
+      erb :tags
+    end
+
+    get '/tag/:slug' do
+      @tag = Tag.find_by_slug(params[:slug])
+      redirect '/tags' unless @tag
+
+      title.prepend 'tags', @tag.name
+      erb :tag
+    end
+
+    get '/page/:page' do
+      title.append Blurt.configuration.tagline
+      @posts = Post.for_page(params[:page])
+      erb :posts
+    end
+
+    get '/' do
+      title.append Blurt.configuration.tagline
+      @posts = Post.for_page(1)
+      erb :posts
+    end
+
+    post '/admin' do
+      content_type 'text/xml'
+
+      handler = Blurt::RequestHandler.new(request.body.read)
+      handler.response
+    end
+
+    get '/:slug' do
+      @post = Post.find_by_slug(params[:slug])
+      redirect '/' unless @post
+
+      title.prepend @post.title
+      erb :post
+    end
   end
-end
-
-class Sinatra::Application
-  
-  def title
-    @title ||= Title.new
-  end
-  
-end
-
-get '/uploads/*' do |path_to_file|
-  full_path          = "#{Blurt.configuration.upload_path}/#{path_to_file}"
-  send_file full_path, :disposition => nil
-end
-
-get '/feed' do
-  content_type 'application/xml'
-
-  @posts = Post.for_page(1)
-  builder :posts
-end
-
-get '/sitemap.xml' do
-  content_type 'text/xml'
-  
-  sitemap = Sitemap.new
-  sitemap.to_xml
-end
-
-get '/tags' do
-  title.prepend 'tags'
-  @tags = Tag.by_name
-  erb :tags
-end
-
-get '/tag/:slug' do
-  @tag = Tag.find_by_slug(params[:slug])
-  redirect '/tags' unless @tag
-  
-  title.prepend 'tags', @tag.name
-  erb :tag
-end
-
-get '/page/:page' do
-  title.append Blurt.configuration.tagline
-  @posts = Post.for_page(params[:page])
-  erb :posts
-end
-
-get '/' do
-  title.append Blurt.configuration.tagline
-  @posts = Post.for_page(1)
-  erb :posts
-end
-
-post '/admin' do
-  content_type 'text/xml'
-  
-  handler = Blurt::RequestHandler.new(request.body.read)
-  handler.response
-end
-
-get '/:slug' do
-  @post = Post.find_by_slug(params[:slug])
-  redirect '/' unless @post
-  
-  title.prepend @post.title
-  erb :post
 end
